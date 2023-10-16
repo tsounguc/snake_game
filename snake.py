68e081f3a18133e7c329e36616939961c55d3267
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-15 23:50:27 -0400

    Control snake with keypress

--- snake.py
+++ snake.py
@@ -1,12 +1,17 @@
 from turtle import Turtle
 
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
     def __init__(self):
         self.snake_segments = []
         self.create_snake()
+        self.head = self.snake_segments[0]
 
     def create_snake(self):
         for index in range(3):
@@ -22,3 +27,19 @@ class Snake:
             new_y = self.snake_segments[seg_num - 1].ycor()
             self.snake_segments[seg_num].goto(new_x, new_y)
         self.snake_segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
